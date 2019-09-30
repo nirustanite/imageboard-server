68fe89dbc96a67c9dad43c32a98f4a4f2cac7dcd
nirustanite <nirustanite@gmail.com>
2019-09-30 12:19:53 +0200

    Added post request for creating a data in db

--- image/router.js
+++ image/router.js
@@ -3,6 +3,17 @@ const Image = require('./model');
 
 const router = new Router();
 
+router.post('/image', (req,res,next) => {
+    Image.create(req.body)
+         .then(image => {
+             if(!image){
+                 return res.status(404).end()
+             }
+             return res.json(image)
+         })
+         .catch(next)
+});
+
 router.get('/image', (req,res,next) => {
     Image.findAll()
          .then(images => {
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const imageRouter = require('./image/router')
 const bodyParseer = require('body-parser');
+const cors = require('cors')
 
 const app = express();
 
 const port = process.env.PORT || 4000;
 
 app.use(bodyParseer.json())
+app.use(cors());
 app.use(imageRouter);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
